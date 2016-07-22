93b2cde858282a83123a2d1694037a066d84bb0d
fyrebite <tomhock95@gmail.com>
2016-07-22 13:10:19 +0100

    refactor header

--- header.php
+++ header.php
@@ -11,59 +11,59 @@
 </head>
 
 <body>
-        <nav id="menu">
-            <ul>
-                <li><a href="index.php" title="Home">Home</a></li>
-                <li><a href="about.php" title="About">About</a></li>
-                <li><a href="work.php" title="Work">Work</a></li>
-                <li><a href="services.php" title="Services">Services</a></li>
-                <li><a href="blog.php" title="Blog">Blog</a></li>
-                <li><a href="contact.php" title="Contact">Contact</a></li>
-                <li><hr /></li>
-                <li class="social-links">
-                    <ul>
-                       <li class="contact-link">
-                            <a href="mailto:hello@absolute-agency.co.uk" title="Email us">
-                                <i class="fa fa-envelope" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="tel:07740988831" title="Call us">
-                                <i class="fa fa-phone" aria-hidden="true"></i>
-                             </a>
-                        </li>
-                    
-                        <li>
-                            <a href="#" title="Twitter">
-                                <i class="fa fa-twitter" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Facebook">
-                                <i class="fa fa-facebook" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Pinterest">
-                                <i class="fa fa-pinterest" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#" title="Instagram">
-                                <i class="fa fa-instagram" aria-hidden="true"></i>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </nav>
+    <!-- off screen navigation -->
+    <nav id="menu">
+        <ul>
+            <li><a href="index.php" title="Home">Home</a></li>
+            <li><a href="about.php" title="About">About</a></li>
+            <li><a href="work.php" title="Work">Work</a></li>
+            <li><a href="services.php" title="Services">Services</a></li>
+            <li><a href="blog.php" title="Blog">Blog</a></li>
+            <li><a href="contact.php" title="Contact">Contact</a></li>
+            <li><hr /></li>
+            <li class="social-links">
+                <ul>
+                   <li class="contact-link">
+                        <a href="mailto:hello@absolute-agency.co.uk" title="Email us">
+                            <i class="fa fa-envelope" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="tel:07740988831" title="Call us">
+                            <i class="fa fa-phone" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Twitter">
+                            <i class="fa fa-twitter" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Facebook">
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Pinterest">
+                            <i class="fa fa-pinterest" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" title="Instagram">
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
 
-        <header>
-            <div class="logo" onclick="location.href='index.php';" style="cursor: pointer;"></div>
-            <a id="icon-menu" title="Toggle Menu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </a>
-        </header>
-    <script src="scripts/main.js"></script>
+    <!-- start of on screen content -->
+    <header>
+        <div class="logo" onclick="location.href='index.php';" style="cursor: pointer;"></div>
+        <a id="icon-menu" title="Toggle Menu">
+            <span></span>
+            <span></span>
+            <span></span>
+        </a>
+    </header>
