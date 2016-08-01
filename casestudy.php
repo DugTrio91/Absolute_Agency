9e208474c8a153a60fc18af9018248057faa078b
fyrebite <tomhock95@gmail.com>
2016-08-01 19:39:39 +0100

    fix broken sql queries

--- casestudy.php
+++ casestudy.php
@@ -17,45 +17,44 @@
 <div class="case-study">
     <div class="cover-gradient"></div>
 
-
     <?php        
 
-        $id = $_GET["ID"];
+        $id = $_GET["id"];
         if(isset($id)){
 
-        $sql = "SELECT * FROM portfoliolist WHERE ID = $id";
-        $row = $db->fetchOne($sql);
+            $sql = "SELECT * FROM portfoliolist WHERE ID = $id";
+            $result = mysqli_query($dbconnect, $sql);
 
-        $title = $row["portfolioTitle"];
+            while($row = mysqli_fetch_array($result)){
 
-        if(!empty($row)){
+                $title = $row["portfolioTitle"];
 
-            //cover
-            $out .= "<div id='container1' class='case-study-container1' style='background: url(" . $row["coverImage"] . ") top no-repeat;'>";
-            $out .= "<div class='cover cover-logo' style='background: url(" . $row["logo"] . ") center no-repeat;'></div>";
-            $out .= "<div class='cover cover-text'><h2>" . $row["portfolioTitle"] . "</h2></div>";
-            $out .= "</div>";
-            //container2
-            $out .= "<div id='container2' class='case-study-container2'>";
-            $out .= "<div class='splitcontainer-upper'><p><i>" . $row["aboutIntro"] . "</i></p></div>";
-            $out .= "<div class='splitcontainer-lower' style='background: url(" . $row["image2"] . ") center no-repeat;'></div>";
-            $out .= "</div>";
-            //container3
-            $out .= "<div id='container3' class='case-study-container3'>";
-            $out .= "<div class='splitcontainer-upper'><h2>About the Client</h2><p>" . $row["aboutClient"] . "</p></div>";
-            $out .= "<div class='splitcontainer-lower' style='background: url(" . $row["image3"] . ") center no-repeat;'></div>";
-            $out .= "</div>";
-            //container4
-            $out .= "<div id='container4' class='case-study-container4 style='background: url(" . $row["image4"] . ") center no-repeat;'></div>";
-            //final
-            $out .= "<div id='container5' class='case-study-container5'>";
-            $out .= "<div class='splitcontainer-upper'><h2>The Design</h2><p>" . $row["aboutDesign"] . "</p></div>";
-            $out .= "<div class='splitcontainer-lower' style='background: url(" . $row["image5"] . ") center no-repeat;'></div>";
-            $out .= "</div>";
-            
-            //output
-            echo $out;
-
+                //cover
+                $out .= "<div id='container1' class='case-study-container1' style='background: url(" . $row["coverImage"] . ") top no-repeat;'>";
+                $out .= "<div class='cover cover-logo' style='background: url(" . $row["logo"] . ") center no-repeat;'></div>";
+                $out .= "<div class='cover cover-text'><h2>" . $row["portfolioTitle"] . "</h2></div>";
+                $out .= "</div>";
+                //container2
+                $out .= "<div id='container2' class='case-study-container2'>";
+                $out .= "<div class='splitcontainer-upper'><p><i>" . $row["aboutIntro"] . "</i></p></div>";
+                $out .= "<div class='splitcontainer-lower' style='background: url(" . $row["image2"] . ") center no-repeat;'></div>";
+                $out .= "</div>";
+                //container3
+                $out .= "<div id='container3' class='case-study-container3'>";
+                $out .= "<div class='splitcontainer-upper'><h2>About the Client</h2><p>" . $row["aboutClient"] . "</p></div>";
+                $out .= "<div class='splitcontainer-lower' style='background: url(" . $row["image3"] . ") center no-repeat;'></div>";
+                $out .= "</div>";
+                //container4
+                $out .= "<div id='container4' class='case-study-container4 style='background: url(" . $row["image4"] . ") center no-repeat;'></div>";
+                //final
+                $out .= "<div id='container5' class='case-study-container5'>";
+                $out .= "<div class='splitcontainer-upper'><h2>The Design</h2><p>" . $row["aboutDesign"] . "</p></div>";
+                $out .= "<div class='splitcontainer-lower' style='background: url(" . $row["image5"] . ") center no-repeat;'></div>";
+                $out .= "</div>";
+                    
+                //output
+                echo $out;
+        
             }
 
         }
@@ -81,9 +80,9 @@
             
         <?php 
 
-            $sql = "SELECT * FROM portfolioList ORDER BY RAND() limit 0,3";
+            $sql = "SELECT * FROM portfoliolist ORDER BY RAND() LIMIT 3";
 			$result = mysqli_query($dbconnect, $sql);
-            shuffle($result);
+
             while($row = mysqli_fetch_array($result)){
                     
                 echo "<a href=\"casestudy.php?id=" . $row["ID"] . "\">";
@@ -93,6 +92,7 @@
                 echo "</div>";
                 echo "<h4>" . $row["portfolioText"] . "</h4></a>";
                 echo "</div>";
+
             }
 
         ?>
@@ -101,9 +101,4 @@
     </div>
 </div>
 
-
 <?php include_once "footer.php"; ?>
-        
-
-            
-        
